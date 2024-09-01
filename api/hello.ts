70a24e0a819d0e8e69819564bedd39bc241de243
KeenanKistoo <kistook19@gmail.com>
2024-09-01 22:32:29 +0200

    Added comments

--- api/hello.ts
+++ api/hello.ts
@@ -3,7 +3,11 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 export default function handler(req: VercelRequest, res: VercelResponse) {
   // Get the 'data' field from the request body
   let { data = 'example' } = req.body; 
+
+  //Split the input word into an array
   let array = data.split('');
+
+  //OrdeR the array in alphabetical order
   let finalResponse = array.sort();
 
   
