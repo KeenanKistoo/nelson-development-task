319148a43ff17da69fe8d8d6675a905089fd1c0b
KeenanKistoo <kistook19@gmail.com>
2024-09-01 22:05:32 +0200

    Test 3

--- api/hello.ts
+++ api/hello.ts
@@ -4,9 +4,11 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
   // Get the 'data' field from the request body
   let { data = 'example' } = req.body; 
   let array = data.split('');
+  let finalResponse = array.sort();
+
   
   // Return the data in a JSON response
   return res.json({
-    message: array,
+    message: finalResponse,
   });
 }
