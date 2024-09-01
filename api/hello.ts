5df13e4ea5df0f92fcf76c566574d6f4680009f9
KeenanKistoo <kistook19@gmail.com>
2024-09-01 21:56:03 +0200

    Test 2

--- api/hello.ts
+++ api/hello.ts
@@ -2,8 +2,9 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   // Get the 'data' field from the request body
-  const { data = 'example' } = req.body; 
+  let { data = 'example' } = req.body; 
 
+  data = `${data} Kistoo `
   // Return the data in a JSON response
   return res.json({
     message: `Hello ${data}!`,
